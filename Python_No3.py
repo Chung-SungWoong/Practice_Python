d06901117a25ed6cd3859f2ae9b2e4842640fa59
Paul-Chung27 <dnd8888@naver.com>
2021-09-15 19:35:18 +0900

    Everyday Practice

---
+++ Python_No3.py
@@ -0,0 +1,5 @@
+myname = 'CHUNG'
+my_name = "정성웅"
+MyName = "SungWoong Chung"
+counter = 1
+Counter = 2
---
+++ Python_No5.py
@@ -0,0 +1,11 @@
+#Example of Joo Suck
+#Date 2021 09 15
+a = 1        #set 1 to a
+b = 5        #set 5 to b
+print(a+b)   #Print the answer of a + b
+
+"""
+a = 1
+b = 5
+print(a + b)
+"""
