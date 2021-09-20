95c3044f35acee394520abcacab39b4ade013076
Chung-SungWoong <dnd8888@naver.com>
2021-09-20 15:19:54 +0900

    2021-09-20 시퀀스 개념

---
+++ Python_No28.py
@@ -0,0 +1,15 @@
+"""
+시퀀스 자료 슬라이싱 이해하기
+"""
+
+strdata = "Time is money!!"
+print(strdata[1:5])
+print(strdata[:7])
+print(strdata[9:])
+print(strdata[:-3])
+print(strdata[-3:])
+print(strdata[:])
+print(strdata[::2])
+
+# [시작 인덱스:끝 인덱스:스텝]  
+# 스텝은 자료를 취하는 간격
---
+++ Python_No30.py
@@ -0,0 +1,9 @@
+"""
+시퀀스 자료 반복 이해하기
+"""
+
+artist = 'BTS!'
+fans = "ARMY"
+dispdata = fans + '들이 외칩니다' + artist*3
+
+print(dispdata)
