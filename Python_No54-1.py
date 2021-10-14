f658fdff6677ab4e48c73b7bbac50834921ca29e
정성웅 <chung_sungwoong@jeongseong-ung-ui-MacBookPro.local>
2021-10-14 16:04:48 +0900

    Class heritage

---
+++ Python_No54-1.py
@@ -0,0 +1,18 @@
+"""
+클래스 다중상속
+"""
+class Add:
+    def add(self,n1,n2):
+        return n1 + n2
+
+class Multiply:
+    def multiply(self,n1,n2):
+        return n1*n2
+
+class Calculator(Add,Multiply):
+    def sub(self,n1,n2):
+        return n1-n2
+
+obj = Calculator()
+print(obj.add(1,2))
+print(obj.multiply(3,2))
