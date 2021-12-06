f402e4307e7a9c3cf78791ad91cc2fcde7493804
Chung SungWoong <dnd8888@naver.com>
2021-12-06 22:48:56 +0900

    find a place of the specific word (find)

---
+++ Python_No92.py
@@ -0,0 +1,12 @@
+"""
+문자열에서 특정 문자(열) 위치 찾기 (find)
+"""
+
+txt = 'A lot of things occur each day, every day'
+offset1 = txt.find('e')
+offset2 = txt.find('day')
+offset3 = txt.find('day',30)
+
+print(offset1)
+print(offset2)
+print(offset3)
