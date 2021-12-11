8a4d03a20d1f1e0a179b152cdfa5911b9e17ee9f
Chung SungWoong <dnd8888@naver.com>
2021-12-11 22:38:00 +0900

    encode the string

---
+++ Python_No96.py
@@ -0,0 +1,14 @@
+"""
+문자열을 바이트 객체로 바꾸기(encode)
+"""
+
+u_txt = "I love python"
+b_txt = u_txt.encode()
+print(u_txt)
+print(b_txt)
+
+ret1 = 'I' == u_txt[0]
+ret2 = 'I' == b_txt[0]
+print(ret1)
+print(ret2)
+
