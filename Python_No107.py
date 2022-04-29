d268cb75477c14a89d0d882b7912f02372afe82c
Chung SungWoong <dnd8888@naver.com>
2022-04-29 20:22:24 +0900

    add two list

---
+++ Python_No107.py
@@ -0,0 +1,10 @@
+"""
+리스트 합치기(+)
+"""
+
+listdata = ['a','b','c','d','e']
+listdata2 = ['f','g','h','i','j']
+listdata3 = listdata + listdata2
+listdata4 = listdata2 + listdata
+print(listdata3)
+print(listdata4)
