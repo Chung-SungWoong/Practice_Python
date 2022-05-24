5622a670b9b1e790a98110718d1bdefdb6934b2f
Chung SungWoong <dnd8888@naver.com>
2022-05-24 22:10:48 +0900

    get all the values in the dictionary

---
+++ Python_No129.py
@@ -0,0 +1,9 @@
+"""
+사전 요소를 모두 추출하기
+"""
+names = {'Marry':10999,'Sams':2111,'Aimy':9778,'Tom':20245,'Michale':27115,'Bob':5887,'Kelly':7855}
+items = names.items()
+print(items)
+
+for item in items:
+    print(item)
