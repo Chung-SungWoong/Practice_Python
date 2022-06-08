2e8ab96cdf53478e047da308a9b2d6ad715c1c46
Chung SungWoong <dnd8888@naver.com>
2022-06-08 20:34:21 +0900

    copy the binary file

---
+++ Python_No143.py
@@ -0,0 +1,16 @@
+"""
+바이너리 파일 복사하기
+"""
+
+bufsize = 1024
+f = open('img_sample.jpg','rb')
+h = open('img_sample_copy.jpg','wb')
+
+data = f.read(bufsize)
+while data:
+    h.write(data)
+    data = f.read(bufsize)
+
+f.close()
+h.close()
+
