77cf7a913e97bc2dcc9964e0c015678f77d9961e
Chung SungWoong <dnd8888@naver.com>
2022-06-18 22:22:39 +0900

    make new directory

---
+++ Python_No152.py
@@ -0,0 +1,10 @@
+"""
+디렉터리 생성하기 (os.mkdir)
+"""
+import os 
+newfolder =input('새로 생성할 디렉터리 이름을 입력하세요: ')
+try:
+    os.mkdir(newfolder)
+    print('[%s] 디렉토리를 새로 생성했습니다.' %newfolder)
+except Exception as e:
+    print(e)
