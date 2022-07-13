1ada4e9c12ff3d697bad25175b729002fc63a4d9
Chung SungWoong <dnd8888@naver.com>
2022-07-13 20:50:38 +0900

    Compress file as a zip file

---
+++ Python_No177.py
@@ -0,0 +1,16 @@
+"""
+디렉터리를 하나의 ZIP 파일로 압축하기
+"""
+from zipfile import *
+import os
+
+def compressAll(zipname,folder):
+    print('[%s] -> [%s] 압축...' %(folder,zipname))
+    with ZipFile(zipname,'w') as ziph:
+        for dirname, subdirs, files in os.walk(folder):
+            for file in files:
+                ziph.write(os.path.join(dirname,file))
+
+folder = 'tmp'
+zipname = folder +'.zip'
+compressAll(zipname,folder)
