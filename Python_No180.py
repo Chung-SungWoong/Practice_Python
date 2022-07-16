f27370816a6f369f42af5bbf5a65ab5b92a0cf71
Chung SungWoong <dnd8888@naver.com>
2022-07-16 22:08:49 +0900

    Program that decide the partner

---
+++ Python_No180.py
@@ -0,0 +1,14 @@
+"""
+남녀 파트너 정해주기 프로그램 만들기(zip)
+"""
+
+from random import shuffle
+
+male = ['슈퍼맨','심봉사','로미오','이몽룡','마루치']
+female = ['원더우먼','뺑덕','줄리엣','성춘향','아라치']
+shuffle(male)
+shuffle(female)
+couples = zip(male,female)
+
+for i, couple in enumerate(couples):
+    print('커플%d: [%s]-[%s]' %(i+1,couple[0],couple[1]))
