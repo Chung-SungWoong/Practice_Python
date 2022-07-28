f68c80da76ec977d5e85cedc81b4ae089bbfe2ce
Chung SungWoong <dnd8888@naver.com>
2022-07-28 21:36:55 +0900

    add bullet to the game

--- Python_No191.py
+++ Python_No191.py
@@ -1,7 +1,9 @@
 """
-간단한 슈팅게임 만들기2
+간단한 슈팅게임 만들기 4
 """
-from time import clock_getres
+
+
+import random
 import pygame
 
 BLACK = (0,0,0)
@@ -9,6 +11,8 @@ pad_width = 480
 pad_height = 640
 fighter_width = 36
 fighter_height = 38
+enemy_width = 26
+enemy_height = 20
 
 def drawObject(obj,x,y):
     global gamepad
@@ -16,12 +20,19 @@ def drawObject(obj,x,y):
 
 
 def runGame():
-    global gampad, clock, fighter
+    global gampad, clock, fighter, enemy, bullet
+
+    bullet_xy = []
 
     #전투기 초기 위치 설정하기
     x = int(pad_width*0,45)
     y = int(pad_height*0.9)
     x_change = 0
+
+    # 적 초기 위치 설정
+    enemy_x = random.randrange(0,pad_width-enemy_width)
+    enemy_y = 0
+    enemy_speed = 3
 
     ongame = False
     while not ongame:
@@ -35,9 +46,18 @@ def runGame():
                 
                 elif event.key == pygame.K_RIGHT:
                     x_change += 5
+
+                elif event.key == pygame.K_LCTRL:
+                    if len(bullet_xy) < 2:
+                        bullet_x = x + fight_width/2
+                        bullet_y = y - fight_height
+                        bullet_xy.append([bullet_x,bullet_y])
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+
+            
         
         gamepad.fill(BLACK)
 
@@ -50,18 +70,42 @@ def runGame():
 
         drawObject(fighter,x,y)
 
+        if len(bullet_xy) != 0:
+            for i, bxy in enumerate(bullet_xy):
+                bxy[1] -= 10
+                bullet_xy[i][1] = bxy[1]
+
+            if bxy[1] <= 0:
+                try:
+                    bullet_xy.remove(bxy)
+                except:
+                    pass
+
+        if len(bullet_xy) != 0:
+            for bx, by in bullet_xy:
+                drawObject(bullet, bx, by)
+
+        enemy_y += enemy_speed
+        if enemy_y > pad_height:
+            enemy_y = 0
+            enemy_x = random.randrange(0,pad_width-enemy_width)
+        
+        drawObject(enemy,enemy_x,enemy_y)
+
         pygame.display.update()
         click.tick(60)
     
     pygame.quit()
 
 def initGame():
-    global gamepad, clock, fighter
+    global gamepad, clock, fighter, enemy, bullet 
 
     pygame.init()
     gamepad = pygame.display.set_mode((pad_width,pad_height))
     pygame.display.set_caption('MyGalaga')
     fighter = pygame.image.load('fighter.png')
+    enemy = pygame.image.load('enemy.png')
+    bullet = pygame.image.load('bullet.jpg')
     clock = pygame.time.Clock()
 
 initGame()
