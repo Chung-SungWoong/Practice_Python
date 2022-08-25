68393dc355ab0689c741dad8d3f3c274113f9a3c
Chung SungWoong <dnd8888@naver.com>
2022-08-25 17:12:51 +0900

    round in the decimal

---
+++ Python_Test6.py
@@ -0,0 +1,25 @@
+"""
+1023.3456 값을 소수점 이하 세자리에서 반올림하여 소수점 둘째 자리까지 출력하시오
+"""
+"""
+n = 1023.3456
+l = []
+number = str(n)
+
+for i in range()
+
+
+for i in range(len(number)):
+    print(number[i])
+    if "." in number:
+        i = i + 2
+        if int(number[i]) > 4:
+            int(number[i-1]) + 1
+
+"""
+
+num = 1024.3456
+
+number = round(num,2)   # Or
+print(number)           #
+print("%.2f" %round(num,2))
