11860caa3a902212caa9797ee7d3e9ce209c8ff0
Chung SungWoong <dnd8888@naver.com>
2022-09-03 18:54:56 +0900

    output examples

---
+++ Python_Test17.py
@@ -0,0 +1,10 @@
+"""
+사각형의 가로와 세로 길이를 정수로 입력받고, 그 사각형의 넓이를 출력하세요
+"""
+
+height = int(input("세로의 길이: "))
+width = int(input("가로의 길이: "))
+
+area = height * width
+
+print("사각형의 넓이는 %i 입니다" %area)
---
+++ Python_Test18.py
@@ -0,0 +1,8 @@
+"""
+0 ~ 100 까지의 난수 5개를 출력하세요
+"""
+
+import random
+
+for i in range(5):
+    print(random.randint(0,100))
