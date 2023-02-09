3e68920b21de785d9329e4103ea7dcda7e06f191
Chung SungWoong <dnd8888@naver.com>
2023-02-09 23:42:12 +0900

    Extract Email address from websites

--- Project13_Collect_Email_Excel.py
+++ Project13_Collect_Email_Excel.py
@@ -1,8 +1,9 @@
-"""
 
-"""
 
 import re
+import requests
+
+'''
 
 test_string = """
 aaa@bbb.com
@@ -16,4 +17,23 @@ no.kr
 
 result = re.findall(r'[\w\.-]+@[\w\.-]+',test_string)       # 이메일 형식으로 된 것 확인
 
-print(result)
+results = list(set(results))                                # 중복 제거 코드
+
+print(results)
+
+'''
+
+url = 'https://news.v.daum.net/v/20211129144552297'
+
+headers = {
+    'User-Agent':'Mozilla/5.0',
+    'Content-Type':'Text/html;charset=utf-8'
+}
+
+response = requests.get(url,headers=headers)
+
+results = re.findall(r'[\w\.-]+@[\w\.-]+',response.text)
+
+results = list(set(results))
+
+print(results)
