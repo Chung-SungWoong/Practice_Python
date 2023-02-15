c5442cc7e565ea470f516bb17c27d02acdd8657c
Chung SungWoong <dnd8888@naver.com>
2023-02-16 00:25:16 +0900

    Add While function not to unintentionally turn off

--- Project19.py
+++ Project19.py
@@ -1,17 +1,36 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
-URL = 'http://www.google.co.kr/imghp'
-driver.get(url=URL)
-
-driver.implicitly_wait(time_to_wait=10)
-
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
+def Selenium_Test():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
-elem = driver.find_element(By.CSS_SELECTOR,'body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.emcav > div.RNNXgb > div > div.a4bIc > input')
-elem.send_keys("바다")
-elem.send_keys(Keys.RETURN)
+    URL = 'http://www.google.co.kr/imghp'
+    driver.get(url=URL)
+
+    driver.implicitly_wait(time_to_wait=10)
+
+    elem = driver.find_element(By.CSS_SELECTOR,'body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.emcav > div.RNNXgb > div > div.a4bIc > input')
+    elem.send_keys("바다")
+    elem.send_keys(Keys.RETURN)
+
+    elem = driver.find_element_by_tag_name("body")
+    for i in range(60):
+        elem.send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.1)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR,'body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.emcav > div.RNNXgb > div > div.a4bIc > input').click()
+
+        for i in range(60):
+            elem.send_keys(Keys.PAGE_DOWN)
+            time.sleep(0.1)
+    except:
+        pass
+
+    while(True):
+        pass
+
+Selenium_Test()
