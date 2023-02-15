79f58cd7e7066da3374612526483d847a3e07b77
Chung SungWoong <dnd8888@naver.com>
2023-02-15 11:09:23 +0900

    Find the topic from Google picture

--- Project19.py
+++ Project19.py
@@ -7,3 +7,11 @@ URL = 'http://www.google.co.kr/imghp'
 driver.get(url=URL)
 
 driver.implicitly_wait(time_to_wait=10)
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+elem = driver.find_element(By.CSS_SELECTOR,'body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.emcav > div.RNNXgb > div > div.a4bIc > input')
+elem.send_keys("바다")
+elem.send_keys(Keys.RETURN)
