2d1fb4f08449aa722a52db119def32a1dc555ead
Chung SungWoong <dnd8888@naver.com>
2023-02-25 23:56:17 +0900

    Find the element

--- Project20_Collect_Current_Search.py
+++ Project20_Collect_Current_Search.py
@@ -5,7 +5,13 @@ from selenium import webdriver
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-URL = 'https://www.google.co.kr'
+URL = 'https://signal.bz/news'
 driver.get(url=URL)
 driver.implicitly_wait(time_to_wait=10)
 
+naver_results = driver.find_elements_by_css_selector("abody > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.emcav > div.RNNXgb > div > div.a4bIc > input")
+
+naver_list = []
+for naver_result in naver_results:
+    print(naver_result.txt)
+    naver_list.append(naver_result.text)
