97a033b85fcec74b09d44fb7f519fb04ea680692
Chung SungWoong <dnd8888@naver.com>
2023-02-27 23:59:41 +0900

    Find the current search

--- Project20_Collect_Current_Search.py
+++ Project20_Collect_Current_Search.py
@@ -9,9 +9,15 @@ URL = 'https://signal.bz/news'
 driver.get(url=URL)
 driver.implicitly_wait(time_to_wait=10)
 
+driver.find_element(By.CSS_SELECTOR,'#olLiveIssueKeyword > li:nth-child(1) > a > span.txt_rank').click()
+
+nate_result = driver.find_element(By.CSS_SELECTOR,'#search-option > form:nth-child(1) > fieldset > div.issue-kwd > span > a')
+
 naver_results = driver.find_elements_by_css_selector("abody > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.emcav > div.RNNXgb > div > div.a4bIc > input")
 
 naver_list = []
 for naver_result in naver_results:
     print(naver_result.txt)
     naver_list.append(naver_result.text)
+
+
