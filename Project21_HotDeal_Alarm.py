a81b98d46a444a45d276f108bf721f1c28f9c295
Chung SungWoong <dnd8888@naver.com>
2023-03-07 22:28:48 +0900

    Give message when there is a hot deal

--- Project21_HotDeal_Alarm.py
+++ Project21_HotDeal_Alarm.py
@@ -1,7 +1,7 @@
 """
 세일 정보가 뜨면 알람을 알려주는 프로그램
 """
-
+import telegram
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
@@ -16,3 +16,12 @@ urls = driver.find_elements_by_css_selector("#revolution_main_table > tbody > tr
 for i in range(len(titles)):
     print(titles[i].text)
     print(urls[i].get_attribute('herf'))
+
+message= ''
+for i in range(len(titles)):
+    if "김치" in titles[i].text:
+        message = titles[i].text + "\n" + urls[i].get_attribute('herf')
+        print(message)
+        token = ''
+        bot = telegram.Bot(token)
+        bot.sendMessage(chat_id=id,text=message) 
