9a065314ce3c38ec4e16405ed5ddb51a0507a9e4
Chung SungWoong <dnd8888@naver.com>
2023-03-08 23:39:48 +0900

    Project 21 Hot deal Alaram

--- Project21_HotDeal_Alarm.py
+++ Project21_HotDeal_Alarm.py
@@ -4,24 +4,22 @@
 import telegram
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get(url='https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu')
-driver.implicitly_wait(time_to_wait=10)
+while True:
+    try:
+        driver.get(url='https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu')
+        titles = driver.find_elements_by_css_selector("#revolution_main_table > tbody > tr > td:nth-child(3) > table > tbody > tr> td:nth-child(2) > div > a > font")
+        urls = driver.find_elements_by_css_selector("#revolution_main_table > tbody > tr > td:nth-child(3) > table > tbody > tr> td:nth-child(2) > div > a")
+        for i in range(len(titles)):
+            if "김치" in titles[i].text:
+            message = titles[i].text + "\n" + urls[i].get_attribute('herf')
+            print(message)
+            token = ''
+            bot = telegram.Bot(token)
+            bot.sendMessage(chat_id=id,text=message) 
 
-titles = driver.find_elements_by_css_selector("#revolution_main_table > tbody > tr > td:nth-child(3) > table > tbody > tr> td:nth-child(2) > div > a > font")
-urls = driver.find_elements_by_css_selector("#revolution_main_table > tbody > tr > td:nth-child(3) > table > tbody > tr> td:nth-child(2) > div > a")
 
-for i in range(len(titles)):
-    print(titles[i].text)
-    print(urls[i].get_attribute('herf'))
-
-message= ''
-for i in range(len(titles)):
-    if "김치" in titles[i].text:
-        message = titles[i].text + "\n" + urls[i].get_attribute('herf')
-        print(message)
-        token = ''
-        bot = telegram.Bot(token)
-        bot.sendMessage(chat_id=id,text=message) 
+        driver.get(url='https://www.ppomppu.co.kr/zboard.php?id=ppompu')
