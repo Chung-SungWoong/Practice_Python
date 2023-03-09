1f44a458205ee224a137011915250689ce4a9691
Chung SungWoong <dnd8888@naver.com>
2023-03-09 23:08:50 +0900

    Project21 Hot deal alarm

--- Project21_HotDeal_Alarm.py
+++ Project21_HotDeal_Alarm.py
@@ -8,18 +8,25 @@ import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+send+message_list = []
+
 while True:
     try:
         driver.get(url='https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu')
         titles = driver.find_elements_by_css_selector("#revolution_main_table > tbody > tr > td:nth-child(3) > table > tbody > tr> td:nth-child(2) > div > a > font")
         urls = driver.find_elements_by_css_selector("#revolution_main_table > tbody > tr > td:nth-child(3) > table > tbody > tr> td:nth-child(2) > div > a")
+        message = ''
         for i in range(len(titles)):
             if "김치" in titles[i].text:
-            message = titles[i].text + "\n" + urls[i].get_attribute('herf')
-            print(message)
-            token = ''
-            bot = telegram.Bot(token)
-            bot.sendMessage(chat_id=id,text=message) 
-
+                message = titles[i].text + "\n" + urls[i].get_attribute('herf')
+                print(message)
+                send_message_list.append(message)
+                token = ''
+                id = '780238165'
+                bot = telegram.Bot(token)
+                bot.sendMessage(chat_id=id,text=message) 
+        time.sleep(60.0 * 5)
+    except KeyboardInterrupt:
+        break
 
         driver.get(url='https://www.ppomppu.co.kr/zboard.php?id=ppompu')
