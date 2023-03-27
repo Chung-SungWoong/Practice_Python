a3ddde7876f4b350233771bd339166b3b7d8bc7c
Chung SungWoong <dnd8888@naver.com>
2023-03-27 22:39:06 +0900

    Get the data of the crypto currency and save it to the database

--- Project25_Crypto_Currency.py
+++ Project25_Crypto_Currency.py
@@ -1,7 +1,22 @@
 import pyupbit
+import sqlite3
 
+ticker = 'KRW-BTC'
+interval = 'minute1'
+to = '2023-3-20'
+count = 200
+price_now = pyupbit.get_ohlcv(ticker = ticker, interval = interval, to = to, count=count)
+
+db_path = r'/Users/chung_sungwoong/Desktop/Practice/Practice_Python/Crypto_Coin.db'
+
+con = sqlite3.connect(db_path, isolation_level = None)
+price_now.to_sql('BTC', con, if_exists = 'append')
+
+con.close
+"""
 coin_lists = pyupbit.get_tickers(fiat='KRW')
 print(coin_lists)
 
 price_now = pyupbit.get_current_price(["KRW-BTC","KRW-ETH"])
 print(price_now)
+"""
