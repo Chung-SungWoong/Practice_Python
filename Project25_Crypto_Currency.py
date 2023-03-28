6deb93c964fe0b19d659796bdb0300f6c5cf7b0c
Chung SungWoong <dnd8888@naver.com>
2023-03-29 00:54:00 +0900

    Code that read the database

--- Project25_Crypto_Currency.py
+++ Project25_Crypto_Currency.py
@@ -1,6 +1,15 @@
 import pyupbit
 import sqlite3
+import pandas as pd
 
+db_path = r"/Users/chung_sungwoong/Desktop/Practice/Practice_Python/Crypto_Coin.db"
+con = sqlite3.connect(db_path, isolation_level=None)
+
+readed_df = pd.read_sql("SELECT * FROM 'BTC' " , con, index_col = 'index')
+
+print(readed_df)
+
+"""
 ticker = 'KRW-BTC'
 interval = 'minute1'
 to = '2023-3-20'
@@ -13,6 +22,8 @@ con = sqlite3.connect(db_path, isolation_level = None)
 price_now.to_sql('BTC', con, if_exists = 'append')
 
 con.close
+"""
+
 """
 coin_lists = pyupbit.get_tickers(fiat='KRW')
 print(coin_lists)
