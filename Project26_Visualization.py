3a8e29af52d2d6b6b20af2e7f18d0410a2e5485a
Chung SungWoong <dnd8888@naver.com>
2023-04-16 20:58:33 +0900

    Visualize the frequency

--- Project26_Visualization.py
+++ Project26_Visualization.py
@@ -4,7 +4,7 @@ from matplotlib import font_manager, rc
 
 file_path = r'/Users/chung_sungwoong/Desktop/Practice/Practice_Python/excel.xls'
 df_from_excel = pd.read_excel(file_path,engine='openpyxl')              # xlsx가 아닌 csv 파일로 바꿀 수 있으면 zipfile.BadZipFile: File is not a zip file 에러가 나지 않음
-                                                                        # 파일 내용이 깨졌음
+                                                                     # 파일 내용이 깨졌음
 df_from_excel = df_from_excel.drop(index=[0,1])
 
 df_from_excel.columns = ['년도','회차','추첨일','1등당첨자수','1등당첨금액','2등당첨자수','2등당첨금액','3등당첨자수','3등당첨금액','4등당첨자수','4등당첨금액','5등당첨자수','5등당첨금액','당첨번호1','당첨번호2','당첨번호3','당첨번호4','당첨번호5','당첨번호6','보너스번호']
@@ -34,4 +34,30 @@ plt.show()
 """
 print(df_from_excel['회차'].values)
 print(df_from_excel['1등담첨금액'].values)
+"""
+
+import pandas as pd
+from collections import Counter
+
+file_path = r'/Users/chung_sungwoong/Desktop/Practice/Practice_Python/excel.xls'
+df_from_excel = pd.read_excel(file_path,engine='openpyxl') 
+
+df_from_excel = df_from_excel.drop(index=[0,1])
+
+df_from_excel.columns = ['년도','회차','추첨일','1등당첨자수','1등당첨금액','2등당첨자수','2등당첨금액','3등당첨자수','3등당첨금액','4등당첨자수','4등당첨금액','5등당첨자수','5등당첨금액','당첨번호1','당첨번호2','당첨번호3','당첨번호4','당첨번호5','당첨번호6','보너스번호']
+
+num_list = list(df_from_excel['당첨번호1'].astype(int))
+num_list += list(df_from_excel['당첨번호2'].astype(int))
+num_list += list(df_from_excel['당첨번호3'].astype(int))
+num_list += list(df_from_excel['당첨번호4'].astype(int))
+num_list += list(df_from_excel['당첨번호5'].astype(int))
+num_list += list(df_from_excel['당첨번호6'].astype(int))
+
+count = Counter(num_list)
+most_num = count.most_common(45)
+
+print(most_num)
+
+"""
+당첨 횟수의 빈도수를 출력하는 코드 만들기
 """
