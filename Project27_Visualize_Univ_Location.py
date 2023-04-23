5bbcf0245dc6639a3ed5c09b5c07954fe8bbcef5
Chung SungWoong <dnd8888@naver.com>
2023-04-23 22:59:24 +0900

    Project27 University Location

--- Project27_Visualize_Univ_Location.py
+++ Project27_Visualize_Univ_Location.py
@@ -4,6 +4,14 @@
 
 import pandas as pd
 import requests
+from openpyxl import load_workbook
+from openpyxl import Workbook
+import re
+
+filePath = r'/Users/chung_sungwoong/Desktop/Practice/Practice_Python/고등교육기관 주소록(2022).xlsx'
+df_from_excel = pd.read_excel(filePath,engine='openpyxl')
+df_from_excel.columns = df_from_excel.loc[4].tolist()
+df_from_excel = df_from_excel.drop(index=list(range(0,5)))
 
 url = 'http://api.vworld.kr/req/address?'
 params = 'service=address&request=getcoord&version=2.0&crs=epsg:4326&refine=true&simple=false&format=json&type='
@@ -23,20 +31,20 @@ def request_geo(road):
     else:
         x = 0
         y = 0
-        return 0
+        return x,y
     
-x,y = request_geo('경기도 시흥시 산기대학로 237 (정왕동, 한국산업기술대학교)')
+try:
+    wb = load_workbook(r"/Users/chung_sungwoong/Desktop/Practice/Practice_Python/고등교육기관 주소록(2022).xlsx",data_only=True)
+    sheet = wb.active
+except:
+    wb = Workbook()
+    sheet = wb.active
 
-print(f'x값: {x}')
-print(f'y값: {y}')
+university_list = df_from_excel['학교명'].to_list()
+address_list = df_from_excel['주소'].to_list()
 
 """
-filePath = r'/Users/chung_sungwoong/Desktop/Practice/Practice_Python/고등교육기관 주소록(2022).xlsx'
-df_from_excel = pd.read_excel(filePath,engine='openpyxl')
 
-df_from_excel.columns = df_from_excel.loc[4].tolist()
-
-df_from_excel = df_from_excel.drop(index=list(range(0,5)))
 
 print(df_from_excel.head())
 
