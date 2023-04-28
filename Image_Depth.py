9e014041baf60c6c24ed215f42479b2f26609a13
Chung SungWoong <dnd8888@naver.com>
2023-04-28 20:49:43 +0900

    Find the depth of the image

---
+++ Image_Depth.py
@@ -0,0 +1,10 @@
+import sys
+
+from helpers import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+im = load_image('/Users/chung_sungwoong/Desktop/Image Processing/Class_3/test02_a.png')
+
+print(im.dtype)
