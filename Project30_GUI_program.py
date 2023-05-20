3ddbd4e0e74ba58939a416f85c0cd8702000ba38
Chung SungWoong <dnd8888@naver.com>
2023-05-20 23:55:21 +0900

    Increase the size of the font:

--- Project30_GUI_program.py
+++ Project30_GUI_program.py
@@ -1,11 +1,14 @@
 import tkinter
+import tkinter.font
 
 window = tkinter.TK()
 window.title('가상화폐 금액표시')
 window.geometry("400x200")
 window.resizable(False,False)
 
+font = tkinter.font.Font(size = 30)
 label=tkinter.Label(window, text="hello")
+
 label.pack()
 
 window.mainloop()
