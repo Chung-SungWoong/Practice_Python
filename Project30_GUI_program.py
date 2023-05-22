512ed1254fe60dcf0a0ccde2bba35ba0bd1025d3
Chung SungWoong <dnd8888@naver.com>
2023-05-22 22:55:48 +0900

    Display the price of the crypto currency

--- Project30_GUI_program.py
+++ Project30_GUI_program.py
@@ -1,5 +1,19 @@
 import tkinter
 import tkinter.font
+import pyupbit
+import threading
+import time
+
+coin_price = 0
+def get_coin_price():
+    global coin_price
+    while True:
+        coin_price = pyupbit.get_current_price("KRW-BTC")
+        time.sleep(1.0)
+
+t1 = threading.Thread(target=get_coin_price)
+t1.daemon = True
+t1.start()
 
 window = tkinter.TK()
 window.title('가상화폐 금액표시')
@@ -7,15 +21,12 @@ window.geometry("400x200")
 window.resizable(False,False)
 
 font = tkinter.font.Font(size = 30)
-label=tkinter.Label(window, text="hello")
-
+label=tkinter.Label(window, text=" ",font = font)
 label.pack()
 
-cnt = 0
 def get_coin_1sec():
-    global cnt
-    now_btc_price = str(cnt)
-    cnt = cnt + 1
+    global coin_price
+    now_btc_price = str(coin_price)
     label.config(text=now_btc_price)
     window.after(1000,get_coin_1sec)
 
