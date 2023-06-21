f90cd3d2cae79cfe590fe64adb10f65a0535ba84
Chung SungWoong <dnd8888@naver.com>
2023-06-22 00:24:48 +0900

    Painting tool:

--- Project34.Drawing_Tool.py
+++ Project34.Drawing_Tool.py
@@ -10,6 +10,33 @@ class WindowClass(QMainWindow,form_class):
         super().__init__()
         self.setupUi(self)
 
+        self.brushcolor = Qt.black
+
+        self.canvas = QtGui.QPixmap(self.lb_canvas.width(),self.lb_canbas.height())
+        self.canvas.fill(QtGui.QClolor("white"))
+        self.lb_canvas.setPixmap(self.canvas)
+        
+        self.btn_black.clicked.connect(self.btn_clicked)
+        self.btn_black.setStyleSheet('background:black')
+        
+        self.btn_red.clicked.connect(self.btn_clicked)
+        self.btn_red.setStyleSheet('background:red')
+
+        self.btn_blue.clicked.connect(self.btn_clicked)
+        self.btn_blue.setStyleSheet('background:blue')
+
+        self.btn_clear.clicked.connect(self.btn_clear_clicked)
+
+    def btn_clicked(self):
+        btn_value = self.sender().objectName()
+        print(btn_value)
+
+    def btn_clear_clikced(self):
+        print("모두지움")
+
+    def mouseMoveEvent(self,e):
+        print(e.x(),e.y())
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = WindowClass()
