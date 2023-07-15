52db4caba33e98caca4320586c87b870cc949bca
Chung SungWoong <dnd8888@naver.com>
2023-07-15 18:26:18 +0900

    Update the game

--- Project35_JumpGame.py
+++ Project35_JumpGame.py
@@ -9,6 +9,30 @@ pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((MAX_WIDTH,MAX_HEIGHT))
 
+class Player():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.isJump = False
+        self.jupCount = 10
+
+    def draw(self):
+        return pygame.draw.rect(screen,(0,0,255),(self.x,self.y,40,40))
+    
+    def jump(self):
+        if self.isJump:
+            if self.jumpCount >= -10:
+                neg = 1
+                if self.jumpCount < 0:
+                    neg = -1
+                self.y -= self.jumpCount ** 2 * 0.7 * neg
+                self.jumpCount -= 1
+            else:
+                self.isJump = False
+                self.jumpCount = 10
+
+player = Player(50,MAX_HEIGHT - 40)
+
 def main():
     while True:
         for event in pygame.event.get():
@@ -17,11 +41,17 @@ def main():
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    print("space")
+                    player.isJump = True
+            
 
             clock.tick(FPS)
             screen.fill((255,255,255))
 
+            player_rect = player.draw()
+            player.jump()
+
+            print(player_rect)
+            
             pygame.display.update()
 
 if __name__ == '__main__':
