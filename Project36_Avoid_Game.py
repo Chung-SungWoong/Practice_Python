c1c19630e18fd455d2c692e92a0f763b213c1f91
Chung SungWoong <dnd8888@naver.com>
2023-07-24 00:05:41 +0900

    Projedt 36 Avoid Game

--- Project36_Avoid_Game.py
+++ Project36_Avoid_Game.py
@@ -19,16 +19,33 @@ class Player():
     def draw(self):
         return pygame.draw.rect(screen,(0,0,255),(self.x,self.y,40,40))
     
-    if pressed_keys[K_RIGHT]:
-        if self.x < MAX_WIDTH - 40:
-            self.x += 
-
     def move(self):
         if pressed_keys[K_RIGHT]:
-            if self.x < MAX_WIDTH-40:
+            if self.x < MAX_WIDTH - 40:
                 self.x += 5
+
+        if pressed_keys[K_LEFT]:
+            if self.x > 0:
+                self.x -= 5
     
+class Enemy():
+    def __init__(self):
+        self.x = random.randrange(0,MAX_WIDTH-40)
+        self.y = 50
+        self.speed = random.randrange(10,20)
+    
+    def draw(self):
+        return pygame.draw.rect(screen,(255,0,0),(self.x,self.y,20,20))
+    
+    def move(self):
+        self.y = self.y + self.speed
+        if self.y >= MAX_HEIGHT:
+            self.y = 50
+            self.x = range.randrange(0,MAX_WIDTH - 40)
+            self.speed = random.randrange(7,15)
+
 player = Player(MAX_WIDTH/2,MAX_HEIGHT - 40)
+enemy = Enemy()
 
 def main():
     while True:
@@ -44,7 +61,16 @@ def main():
         player_rect = player.draw()
         player.move()
 
+        enemy_rect = enemy.draw()
+        enemy.move()
+
+        if player_rect.colliderect(enemy_rect):
+            print("collide")
+            pygame.quit()
+            sys.exit()
+        
         pygame.display.update()
+
 
 if __name__ == '__main__':
     main()
